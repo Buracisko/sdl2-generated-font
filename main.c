31da3c6a5442863d2dcce5b5fd6b7d929262d54d
Jakub Bureš <jak.bures@gmail.com>
2020-12-21 00:29:40 +0100

    CMake setup and initialization of SDL2

---
+++ main.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "SDL.h"
+
+/*
+Compile with:
+	gcc -o hello main.c -I/usr/include/SDL2 `sdl2-config --cflags --libs`
+*/
+
+void printSDLVersion();
+
+int main(int argc, char* args[])
+{
+	// SDL initialization
+	if (SDL_Init(SDL_INIT_VIDEO))
+	{
+		SDL_Log("Unable to initialize SDL: %s", SDL_GetError());
+		return 1;
+	}
+	puts("SDL initialized.");
+
+	printSDLVersion();
+
+	// Cleanup and exit
+	SDL_Quit();
+
+	return 0;
+}
+
+void printSDLVersion()
+{
+	SDL_version compiled;
+	SDL_version linked;
+
+	SDL_VERSION(&compiled);
+	SDL_GetVersion(&linked);
+	printf("Compiled against SDL version %d.%d.%d\n", compiled.major, compiled.minor, compiled.patch);
+	printf("Linking against SDL version %d.%d.%d\n", linked.major, linked.minor, linked.patch);
+}
