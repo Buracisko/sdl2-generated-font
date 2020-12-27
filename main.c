e01914af6cef5fcd0890d374167c83c64d0120eb
Jakub Bureš <jak.bures@gmail.com>
2020-12-27 20:51:48 +0100

    Remove temporary version output

--- main.c
+++ main.c
@@ -6,13 +6,6 @@
 
 #include "SDL.h"
 
-/*
-Compile with:
-	gcc -o hello main.c -I/usr/include/SDL2 `sdl2-config --cflags --libs`
-*/
-
-void printSDLVersion();
-
 int main(int argc, char* argv[])
 {
 	// Needded to deal with windows main function linking.
@@ -24,23 +17,9 @@ int main(int argc, char* argv[])
 		SDL_Log("Unable to initialize SDL: %s", SDL_GetError());
 		return 1;
 	}
-	puts("SDL initialized.");
-
-	printSDLVersion();
 
 	// Cleanup and exit
 	SDL_Quit();
 
 	return 0;
-}
-
-void printSDLVersion()
-{
-	SDL_version compiled;
-	SDL_version linked;
-
-	SDL_VERSION(&compiled);
-	SDL_GetVersion(&linked);
-	printf("Compiled against SDL version %d.%d.%d\n", compiled.major, compiled.minor, compiled.patch);
-	printf("Linking against SDL version %d.%d.%d\n", linked.major, linked.minor, linked.patch);
 }
